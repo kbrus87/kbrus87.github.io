4cc3ad7c2e241d025d178ffcf39cb97a3c995f95
Bruno Ramari <bjramari@hotmail.com>
2025-04-22 09:44:04 -0300

    headers

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import axios from "axios";
 
 import { MyProvider } from "./context/Context";
+
+axios.interceptors.request.use((request) => {
+  request.headers.authorization = `Bearer ${window.localStorage.getItem(
+    "token"
+  )}`;
+  request.headers["x-frontend-origin"] = "kbrus"
+  return request;
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
